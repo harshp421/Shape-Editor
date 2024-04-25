496aa6bc777db367a83bfd051b833b9107c09974
Yash-1511 <yashp3020@gmail.com>
2024-04-25 19:33:15 +0530

    ADD: final commit

--- svgEditor/src/filters/availableFilters.tsx
+++
@@ -1,9 +0,0 @@
-import { OUTLINE_FILTER } from './outline.filter';
-import { INSET_SHADOW_FILTER } from './insetShadow.filter';
-
-export type AVAILABLE_FILTERS = OUTLINE_FILTER | INSET_SHADOW_FILTER;
-
-export enum FILTER_TYPES {
-    OUTLINE = 'outline',
-    INSET_SHADOW = 'insetShadow'
-}
